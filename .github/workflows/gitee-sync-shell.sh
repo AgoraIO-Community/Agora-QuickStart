24704d8c79d01a6144e006e10fc369afe5364ee6
xucz <xgfd3@users.noreply.github.com>
2023-12-25 10:56:12 +0800

    [Android] add china repos gitee sync shell.

---
+++ .github/workflows/gitee-sync-shell.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git config --global user.email "sync2gitee@example.com"
+git config --global user.name "sync2gitee"
+
+pwd
+git remote -v
+
+// change android maven to china repos
+sed -ie "s#google()#maven { url \"https\://maven.aliyun.com/repository/public\" }\n        google()#g" Android/Agora-RTC-QuickStart-Android/settings.gradle
+sed -ie "s#https://services.gradle.org/distributions#https://mirrors.cloud.tencent.com/gradle#g" Android/Agora-RTC-QuickStart-Android/gradle/wrapper/gradle-wrapper.properties
+git add Android/Agora-RTC-QuickStart-Android/settings.gradle Android/Agora-RTC-QuickStart-Android/gradle/wrapper/gradle-wrapper.properties
+git commit -m '[Android] gitee sync >> use china repos.'
+
+git branch
+git status
+git push
